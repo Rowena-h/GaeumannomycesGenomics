57aecfa4165ee86d68ca31aadc64770ade497ff4
Rowena-h <rowena.c.hill@gmail.com>
2023-09-04 16:26:47 +0100

    update path

--- 01_assembly/010_filter_lowcov.sh
+++ 01_assembly/010_filter_lowcov.sh
@@ -8,34 +8,15 @@
 #SBATCH --mail-user=rowena.hill@earlham.ac.uk   # send-to address
 
 strain_file=$(awk '{print $2}' ../strains | sed -n ${SLURM_ARRAY_TASK_ID}p)
-orig_strain=$(echo ${strain_file} | sed 's/_.*$//')
+asm_file=../scratch/hifiasm-assemblies/${strain_file}/${strain_file}.asm.bp.p_ctg.fa
 out_dir=../scratch/010_filter_lowcov/${strain_file}
 
 #Make output directory
 mkdir -p ${out_dir}
 
-#Use contaminant filtered unitigs if they exist, otherwise the contigs
-if [[ -f ../scratch/009_remove_contam/${strain_file}/${strain_file}.asm.bp.p_utg.fa ]]
-then
-	
-	asm_file=../scratch/009_remove_contam/${strain_file}/${strain_file}.asm.bp.p_utg.fa
-
-	#Filter low coverage contigs according to KAT
-	singularity exec ~/singularity_imgs/Bioconductor.img Rscript scripts/low_cov_deleter2.R \
-        	${asm_file} \
-        	../scratch/002_kat_hist/${orig_strain}/${orig_strain}_63mers \
-        	../scratch/009_kat_sect/${strain_file}/${strain_file}_hifi_vs_assm_sect-stats.tsv \
-        	${out_dir}/${strain_file}.asm.bp.p_utg.hifi_sect_filtered.fa
-
-else
-
-        asm_file=../scratch/hifiasm-assemblies/${strain_file}/${strain_file}.asm.bp.p_ctg.fa
-
-	#Filter low coverage contigs according to KAT
-        singularity exec ~/singularity_imgs/Bioconductor.img Rscript scripts/low_cov_deleter2.R \
-                ${asm_file} \
-                ../scratch/002_kat_hist/${orig_strain}/${orig_strain}_63mers \
-                ../scratch/009_kat_sect/${strain_file}/${strain_file}_hifi_vs_assm_sect-stats.tsv \
-                ${out_dir}/${strain_file}.asm.bp.p_ctg.hifi_sect_filtered.fa
-
-fi
+#Filter low coverage contigs according to KAT
+singularity exec ~/programmes/Bioconductor/Bioconductor.img Rscript low_cov_deleter.R \
+	${asm_file} \
+	../scratch/001_kat_hist/${strain_file}/${strain_file}_63mers \
+	../scratch/009_kat_sect/${strain_file}/${strain_file}_hifi_vs_assm_sect-stats.tsv \
+	${out_dir}/${strain_file}.asm.bp.p_ctg.hifi_sect_filtered.fa
