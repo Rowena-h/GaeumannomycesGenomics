3f6edfec2f7fcc75304f8520cb36df30c876ad7d
Rowena-h <rowena.c.hill@gmail.com>
2023-09-20 14:13:45 +0100

    customise KAT plot

--- 01_assembly/011_kat_comp.sh
+++ 01_assembly/011_kat_comp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p ei-long                              # queue
+#SBATCH -p ei-short                             # queue
 #SBATCH -N 1                                    # number of nodes
 #SBATCH -c 16                                   # number of cores
 #SBATCH --mem 100000                            # memory pool for all cores
@@ -13,20 +13,8 @@ ml kat/2.3.4
 strain_file=$(awk '{print $2}' ../strains | sed -n ${SLURM_ARRAY_TASK_ID}p)
 orig_strain=$(echo ${strain_file} | sed 's/_.*$//')
 out_dir=../scratch/011_kat_comp/${strain_file}
-
-#Use contaminant, coverage filtered unitigs if they exist, otherwise the contigs
-if [[ -f ../scratch/010_filter_lowcov/${strain_file}/${strain_file}.asm.bp.p_utg.hifi_sect_filtered.fa ]]
-then
-
-        asm_file=../scratch/010_filter_lowcov/${strain_file}/${strain_file}.asm.bp.p_utg.hifi_sect_filtered.fa
-        reads_file=$(awk '{print $3}' ../strains | sed -n ${SLURM_ARRAY_TASK_ID}p)
-
-else
-
-        asm_file=../scratch/010_filter_lowcov/${strain_file}/${strain_file}.asm.bp.p_ctg.hifi_sect_filtered.fa
-        reads_file=../scratch/hifi-reads/*${strain_file}_hifi.fastq.gz
-
-fi
+asm_file=../scratch/010_filter_lowcov/${strain_file}/${strain_file}.asm.bp.p_ctg.hifi_sect_filtered.fa
+reads_file=../scratch/hifi-reads/*${strain_file}_hifi.fastq.gz
 
 #Make output directory
 mkdir -p ${out_dir}
@@ -35,3 +23,7 @@ kat comp \
 	-m 63 -t ${SLURM_CPUS_PER_TASK} \
 	-o ${out_dir}/${strain_file}_filteredhifiasm_vs_hifi \
 	-H 1000000000 <(gunzip -c ${reads_file}) ${asm_file}
+
+kat plot spectra-cn ${out_dir}/${strain_file}_filteredhifiasm_vs_hifi-main.mx \
+	-x 300 \
+	-o ${out_dir}/${strain_file}_filteredhifiasm_vs_hifi-main.mx.spectra-cn.pdf
