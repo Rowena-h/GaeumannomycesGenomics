db17ee195d196c8afb3e449ef60e240248ae40d3
Rowena-h <rowena.c.hill@gmail.com>
2023-12-04 14:27:02 +0000

    add avenacinase

--- 04_phylogenetic_classification/003_raxmlng_genetree.sh
+++ 04_phylogenetic_classification/003_raxmlng_genetree.sh
@@ -1,55 +1,56 @@
 #!/bin/bash
-#SBATCH -p ei-medium                            # queue
+#SBATCH -p ei-long                              # queue
 #SBATCH -N 1                                    # number of nodes
 #SBATCH -c 2                                    # number of cores
-#SBATCH --mem 10000                             # memory pool for all cores
+#SBATCH --mem 1GB                               # memory pool for all cores
 #SBATCH --output ../scratch/logs/%x.o%j         # STDOUT and STDERR
 #SBATCH --mail-type=end,fail                    # notifications for job done & fail
 #SBATCH --mail-user=rowena.hill@earlham.ac.uk   # send-to address
 
-source raxml-ng-0.8.0
-
 for marker in $(cat markers)
 do
+
+	if [[ ${marker} = avenacinase ]]
+	then
+		model=JTT+I+G4
+	else
+		model=GTR+G
+	fi
+
+	aln=alignments/${marker}_genetree_aln.fasta
 
 	if [[ -f "alignments/${marker}_genetree.fasta" ]]
 	then
 		#Convert file for RAxML-NG
-		raxml-ng --parse \
-	        	 --msa alignments/${marker}_genetree_aln.fasta \
-			 --model GTR+G \
-	        	 --prefix raxmlng/gaeumannomyces_${marker}
+		singularity exec ~/programmes/raxml-ng/raxml-ng-1.1.0.img raxml-ng \
+			--parse \
+	        	--msa ${aln} \
+			--model ${model} \
+	        	--prefix raxmlng/test/gaeumannomyces_${marker}
 
 		if [[ ${marker} = ITS2 ]]
-		then		
-
-			#Run ML tree search and bootstrapping for <=1000 iterations
-			raxml-ng --all \
-				 --msa raxmlng/gaeumannomyces_${marker}.raxml.reduced.phy \
-				 --model GTR+G \
-			         --prefix raxmlng/gaeumannomyces_${marker} \
-			         --seed 2 \
-			         --threads ${SLURM_CPUS_PER_TASK} \
-		        	 --bs-trees autoMRE{1000}
-
+		then
+			msa=raxmlng/gaeumannomyces_${marker}.raxml.reduced.phy
 		else
+			msa=${aln}
+		fi		
 
-			#Run ML tree search and bootstrapping for <=1000 iterations
-	                raxml-ng --all \
-                        	 --msa alignments/${marker}_genetree_aln.fasta \
-                        	 --model GTR+G \
-                        	 --prefix raxmlng/gaeumannomyces_${marker} \
-                        	 --seed 2 \
-                        	 --threads ${SLURM_CPUS_PER_TASK} \
-                        	 --bs-trees autoMRE{1000}			
-
-		fi
+		#Run ML tree search and bootstrapping for <=1000 iterations
+		singularity exec ~/programmes/raxml-ng/raxml-ng-1.1.0.img raxml-ng \
+			--all \
+			--msa ${msa} \
+			--model ${model} \
+			--prefix raxmlng/test/gaeumannomyces_${marker} \
+			--seed 2 \
+			--threads auto{${SLURM_CPUS_PER_TASK}} \
+			--bs-trees autoMRE{1000}			
 
 		#Check convergence
-		raxml-ng --bsconverge \
-		         --bs-trees raxmlng/gaeumannomyces_${marker}.raxml.bootstraps \
-		         --prefix raxmlng/gaeumannomyces_${marker}_convergence_test \
-		         --seed 2
+		singularity exec ~/programmes/raxml-ng/raxml-ng-1.1.0.img raxml-ng \
+			--bsconverge \
+		        --bs-trees raxmlng/test/gaeumannomyces_${marker}.raxml.bootstraps \
+		        --prefix raxmlng/test/gaeumannomyces_${marker}_convergence_test \
+		        --seed 2
 	fi
 
 done
